3d7d160d6c9689ba609f9032c51a88718e1bdc55
hejiafu <hejiafu@secoo.com>
2016-08-11 10:52:30 +0800

    add

--- src/main/java/com/rabbitmq/spring/SpringQueueSender.java
+++ src/main/java/com/rabbitmq/spring/SpringQueueSender.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SpringReceiver {
+public class SpringQueueSender {
 	public static void main(String[] args) throws InterruptedException {
 		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"rabbitmq.xml");
